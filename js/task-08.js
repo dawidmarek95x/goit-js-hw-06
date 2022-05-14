df9561c6b8521f5633a46c0c9f2fcc3a3343b508
dawidmarek95x <dawid.marek95x@gmail.com>
2022-05-14 02:37:53 +0200

    update/nine-tasks-completed

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,18 @@
+const loginForm = document.querySelector(".login-form");
 
+loginForm.addEventListener("submit", submitFunc);
+
+function submitFunc(evt) {
+  evt.preventDefault();
+
+  const {email, password} = evt.currentTarget.elements;
+
+  ((email.value || password.value) === "") 
+  ? alert("Please fill in all the fields.") 
+  : console.log({
+    [email.name]: email.value,
+    [password.name]: password.value,
+  });
+
+  evt.currentTarget.reset();
+};
--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,19 @@
+// A function that generates a random color in HEX format
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const body = document.body;
+const textOutput = document.querySelector('.color');
+const btnChangingColor = document.querySelector(".change-color");
+
+// A function that returns the color of the selected element and the output when you click on the selected element (button)
+function colorChange(input, styledOutput, output) {
+  input.addEventListener("click", (evt) => {
+    styledOutput.style.backgroundColor = getRandomHexColor();
+    output.textContent = getRandomHexColor();
+  });
+};
+
+// Change the color of the <body> element and the text in span.color after clicking the button.change-color button
+colorChange(btnChangingColor, body, textOutput);
